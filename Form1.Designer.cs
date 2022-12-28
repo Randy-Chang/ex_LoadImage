c1159e88ebf846f4502b653c70ce21e3ffff4a46
MSI\Y <gdba5712@gmail.com>
2022-12-28 17:07:36 +0800

    新增可讀取資料夾內的圖片且顯示

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,22 +31,29 @@
             this.btnChooseFilePath = new System.Windows.Forms.Button();
             this.tbFilePath = new System.Windows.Forms.TextBox();
             this.pbImage1 = new System.Windows.Forms.PictureBox();
+            this.tbFolderPath = new System.Windows.Forms.TextBox();
+            this.btnChooseFolderPath = new System.Windows.Forms.Button();
+            this.lbFilePath = new System.Windows.Forms.Label();
+            this.lbFolderPath = new System.Windows.Forms.Label();
+            this.nudOrder = new System.Windows.Forms.NumericUpDown();
+            this.lbFileName = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pbImage1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudOrder)).BeginInit();
             this.SuspendLayout();
             // 
             // btnChooseFilePath
             // 
-            this.btnChooseFilePath.Location = new System.Drawing.Point(385, 31);
+            this.btnChooseFilePath.Location = new System.Drawing.Point(461, 28);
             this.btnChooseFilePath.Margin = new System.Windows.Forms.Padding(5);
             this.btnChooseFilePath.Name = "btnChooseFilePath";
             this.btnChooseFilePath.Size = new System.Drawing.Size(217, 32);
             this.btnChooseFilePath.TabIndex = 0;
-            this.btnChooseFilePath.Text = "Choose FilePath";
+            this.btnChooseFilePath.Text = "Choose File Path";
             this.btnChooseFilePath.UseVisualStyleBackColor = true;
             // 
             // tbFilePath
             // 
-            this.tbFilePath.Location = new System.Drawing.Point(37, 32);
+            this.tbFilePath.Location = new System.Drawing.Point(126, 28);
             this.tbFilePath.Margin = new System.Windows.Forms.Padding(5);
             this.tbFilePath.Name = "tbFilePath";
             this.tbFilePath.Size = new System.Drawing.Size(325, 32);
@@ -55,17 +62,75 @@
             // pbImage1
             // 
             this.pbImage1.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.pbImage1.Location = new System.Drawing.Point(37, 106);
+            this.pbImage1.Location = new System.Drawing.Point(12, 177);
             this.pbImage1.Name = "pbImage1";
             this.pbImage1.Size = new System.Drawing.Size(565, 321);
             this.pbImage1.TabIndex = 2;
             this.pbImage1.TabStop = false;
+            // 
+            // tbFolderPath
+            // 
+            this.tbFolderPath.Location = new System.Drawing.Point(126, 87);
+            this.tbFolderPath.Margin = new System.Windows.Forms.Padding(5);
+            this.tbFolderPath.Name = "tbFolderPath";
+            this.tbFolderPath.Size = new System.Drawing.Size(325, 32);
+            this.tbFolderPath.TabIndex = 3;
+            // 
+            // btnChooseFolderPath
+            // 
+            this.btnChooseFolderPath.Location = new System.Drawing.Point(461, 86);
+            this.btnChooseFolderPath.Margin = new System.Windows.Forms.Padding(5);
+            this.btnChooseFolderPath.Name = "btnChooseFolderPath";
+            this.btnChooseFolderPath.Size = new System.Drawing.Size(217, 32);
+            this.btnChooseFolderPath.TabIndex = 4;
+            this.btnChooseFolderPath.Text = "Choose Folder Path";
+            this.btnChooseFolderPath.UseVisualStyleBackColor = true;
+            // 
+            // lbFilePath
+            // 
+            this.lbFilePath.AutoSize = true;
+            this.lbFilePath.Location = new System.Drawing.Point(6, 31);
+            this.lbFilePath.Name = "lbFilePath";
+            this.lbFilePath.Size = new System.Drawing.Size(86, 24);
+            this.lbFilePath.TabIndex = 5;
+            this.lbFilePath.Text = "File Path";
+            // 
+            // lbFolderPath
+            // 
+            this.lbFolderPath.AutoSize = true;
+            this.lbFolderPath.Location = new System.Drawing.Point(6, 90);
+            this.lbFolderPath.Name = "lbFolderPath";
+            this.lbFolderPath.Size = new System.Drawing.Size(112, 24);
+            this.lbFolderPath.TabIndex = 6;
+            this.lbFolderPath.Text = "Folder Path";
+            // 
+            // nudOrder
+            // 
+            this.nudOrder.Location = new System.Drawing.Point(612, 177);
+            this.nudOrder.Name = "nudOrder";
+            this.nudOrder.Size = new System.Drawing.Size(66, 32);
+            this.nudOrder.TabIndex = 7;
+            // 
+            // lbFileName
+            // 
+            this.lbFileName.AutoSize = true;
+            this.lbFileName.Location = new System.Drawing.Point(12, 150);
+            this.lbFileName.Name = "lbFileName";
+            this.lbFileName.Size = new System.Drawing.Size(109, 24);
+            this.lbFileName.TabIndex = 8;
+            this.lbFileName.Text = "FileName : ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(801, 510);
+            this.Controls.Add(this.lbFileName);
+            this.Controls.Add(this.nudOrder);
+            this.Controls.Add(this.lbFolderPath);
+            this.Controls.Add(this.lbFilePath);
+            this.Controls.Add(this.btnChooseFolderPath);
+            this.Controls.Add(this.tbFolderPath);
             this.Controls.Add(this.pbImage1);
             this.Controls.Add(this.tbFilePath);
             this.Controls.Add(this.btnChooseFilePath);
@@ -74,6 +139,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pbImage1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudOrder)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -84,5 +150,11 @@
         private Button btnChooseFilePath;
         private TextBox tbFilePath;
         private PictureBox pbImage1;
+        private TextBox tbFolderPath;
+        private Button btnChooseFolderPath;
+        private Label lbFilePath;
+        private Label lbFolderPath;
+        private NumericUpDown nudOrder;
+        private Label lbFileName;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -3,13 +3,20 @@ namespace ex_LoadImage
     public partial class Form1 : Form
     {
         string sFilePath = "";
-        Image image;
+        string sFolderPath = "";
+        Image? image;
+
+        Dictionary<string, string> dcImageFilePath = new Dictionary<string, string>();
+        List<string> listKeyWord = new List<string>();
 
         public Form1()
         {
             InitializeComponent();
             btnChooseFilePath.Click += btnChooseFilePath_Click;
             tbFilePath.KeyDown += tbFilePath_KeyDown;
+
+            btnChooseFolderPath.Click += btnChooseFplderPath_Click;
+            nudOrder.ValueChanged += NumericUpDown_ValueChanged;  
         }
 
         #region UI 相關函式
@@ -26,8 +33,19 @@ namespace ex_LoadImage
             {
                 sFilePath = tbFilePath.Text;
 
+                lbFileName.Text = $"FileName : {Path.GetFileNameWithoutExtension(sFilePath)}";
+
                 ShowImage(pbImage1, sFilePath);
             }
+        }
+
+        private void btnChooseFplderPath_Click(object? sender, EventArgs e)
+        {
+            ChooseFolderPath(ref sFolderPath, tbFolderPath);
+
+            RecordImageFilePathFromFolder(sFolderPath, ref listKeyWord, ref dcImageFilePath);
+
+            InitNumericalUpDown(nudOrder, listKeyWord);
         }
         #endregion
 
@@ -39,6 +57,8 @@ namespace ex_LoadImage
                 
                 sFilePath = file.FileName;
                 tb.Text = file.FileName;
+
+                lbFileName.Text = $"FileName : {Path.GetFileNameWithoutExtension(sFilePath)}";
             }
             
         }
@@ -57,11 +77,11 @@ namespace ex_LoadImage
                 image = Image.FromFile(sFilePath);
 
 
-                Bitmap bmp = null;
+                Bitmap? bmp = null;
 
                 ImageZoom(pb, image, ref bmp);
 
-                PictureBoxZoom(pb, image, ref bmp);
+                //PictureBoxZoom(pb, image, ref bmp);
 
                 pb.Image = bmp;
             }
@@ -74,7 +94,7 @@ namespace ex_LoadImage
         }
 
         /// <summary>
-        /// 
+        /// 影像長寬隨著PictureBox長寬做比例改變
         /// </summary>
         /// <param name="pb"></param>
         /// <param name="image"></param>
@@ -96,7 +116,7 @@ namespace ex_LoadImage
         }
 
         /// <summary>
-        /// 
+        /// PictureBox長寬隨著影像長寬做比例改變
         /// </summary>
         /// <param name="pb"></param>
         /// <param name="image"></param>
@@ -119,7 +139,55 @@ namespace ex_LoadImage
         }
 
 
+        private void ChooseFolderPath(ref string sFolderPath, TextBox tb)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
 
+                sFolderPath = folder.SelectedPath;
+                tb.Text = folder.SelectedPath;
+            }
+        }
+
+        private void RecordImageFilePathFromFolder(string sFolderPath, ref List<string> listKeyWord, 
+                                                        ref Dictionary<string, string> dcImageFilePath)
+        {
+            listKeyWord.Clear();
+            dcImageFilePath.Clear();
+
+            string[] sArray = Directory.GetFiles(sFolderPath, "*.jpg");
+
+            for (int i = 0; i < sArray.Length; i++)
+            {
+                listKeyWord.Add(Path.GetFileNameWithoutExtension(sArray[i]));
+
+                dcImageFilePath.Add(Path.GetFileNameWithoutExtension(sArray[i]), sArray[i]);
+            }
+        }
+
+        private void InitNumericalUpDown(NumericUpDown nud, List<string> listKeyWord)
+        {
+            nud.Minimum = 1;
+            nud.Maximum = listKeyWord.Count;
+            nud.Value = 1;
+        }
+
+        private void NumericUpDown_ValueChanged(object? sender, EventArgs e)
+        {
+            if (listKeyWord.Count != 0)
+            {
+                NumericUpDown? nud = (NumericUpDown?)sender;
+
+                int intValue = Convert.ToInt16(nud.Value) - 1;
+
+                string sKeyWord = listKeyWord[intValue];
+                string sFilePath = dcImageFilePath[sKeyWord];
+
+                lbFileName.Text = $"FileName : {sKeyWord}";
+                ShowImage(pbImage1, sFilePath);
+            }
+        }
 
     }
 }
